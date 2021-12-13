2ad2eb025066a5fdf41d289866d4946c35e8b525
Sayyid Khan <sayyidkhan92@hotmail.com>
2021-12-13 13:32:17 +0800

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+
+   
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd docs/.vitepress/dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:sayyidkhan/sayyidkhan.github.io.git master
+
+# if you are deploying to https://sayyidkhan.github.io/vitepress-starter
+git push -f git@github.com:sayyidkhan/vitepress-starter-v2.git master:gh-pages
+
+cd -
