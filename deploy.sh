75e2882a2418ea167410428a2071e190a4ee7ebb
Sayyid Khan <sayyidkhan92@hotmail.com>
2021-12-13 13:39:15 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,9 @@ cd docs/.vitepress/dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
+git config --global user.name "abc"
+git config --global user.email "abc@example.com"
+
 git init
 git add -A
 git commit -m 'deploy'
